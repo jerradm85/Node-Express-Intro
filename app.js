24000b7d6cc752b35802ba8cdf665dec7c88f813
Jerrad Moon <jerradm85@gmail.com>
2020-06-25 21:34:48 -0700

    solved cipher(with magic numbers)

--- app.js
+++ app.js
@@ -29,7 +29,39 @@ app.get('/sum', (req, res) => {
 
 })
 
+app.get('/cipher', (req, res) => {
 
+  const text = req.query.text.toUpperCase();
+  const shift = Math.abs(Number(req.query.shift));
+
+  if (shift > 26) {
+    return res.send("Shift must be 26 or less.");
+  }
+  // const charArray = text.split("");
+  const asciiArr = [];
+
+  for(let i=0; i < text.length; i++) {
+    asciiArr.push(text[i].charCodeAt(0))
+  }
+
+  const shifted = asciiArr.map(i => {
+    if((i + shift) > 90) {
+      return (i + shift - 91) + 65
+    }
+    return (i + shift);
+  })
+
+  const cipher = String.fromCharCode(...shifted);
+
+
+
+  // const asciiArr = charArray.map(i => {
+  //   charArray[i].charCodeAt(0);
+  // })
+
+  res.send([text, shift, asciiArr, shifted, cipher]);
+
+})
 
 
 
